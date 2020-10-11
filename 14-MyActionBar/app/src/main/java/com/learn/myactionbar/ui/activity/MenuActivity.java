9f3d6a95705deb11306128bab106f0c413699742
AnanditaRian19 <65359346+AnanditaRian19@users.noreply.github.com>
2020-10-11 17:54:00 +0700

    MyActionBar

---
+++ 14-MyActionBar/app/src/main/java/com/learn/myactionbar/ui/activity/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.learn.myactionbar.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.learn.myactionbar.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
