b2b125bf1e2464d1e4973b82295ca69900ffd541
AnanditaRian19 <65359346+AnanditaRian19@users.noreply.github.com>
2020-10-11 18:45:48 +0700

    MyNavigationDrawer

---
+++ 15-MyNavigationDrawer/app/src/main/java/com/learn/mynavigationdrawer/ui/cart/CartFragment.java
@@ -0,0 +1,21 @@
+package com.learn.mynavigationdrawer.ui.cart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.learn.mynavigationdrawer.R;
+
+public class CartFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+}
---
+++ 15-MyNavigationDrawer/app/src/main/java/com/learn/mynavigationdrawer/ui/subway/SubwayActivity.java
@@ -0,0 +1,16 @@
+package com.learn.mynavigationdrawer.ui.subway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.learn.mynavigationdrawer.R;
+
+public class SubwayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subway);
+    }
+}
